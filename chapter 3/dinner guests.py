ca04296aba2286fb860818db0363e950447c2ecd
KeikantsengMokgophe <keikantsengmogomotsi7@gmail.com>
2026-06-09 13:24:32 +0200

    chapter4

---
+++ chapter 3/dinner guests.py
@@ -0,0 +1,5 @@
+# Dinner Guests - Using len()
+
+guests = ["Elon Musk", "Nikola Tesla", "Marie Curie", "Albert Einstein", "Ada Lovelace"]
+
+print(f"I am inviting {len(guests)} people to dinner.")
---
+++ chapter 4/dimentions.py
@@ -0,0 +1,16 @@
+dimentions = (200, 50)
+# print(dimentions[0])
+# print(dimentions[1])
+
+# dimentions[0] = 250
+
+my_t = (3,)
+print("\nfor loop:")
+
+for dimention in dimentions:
+    print(dimention)
+
+    dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+ print(dimension)
